086925722f04b66e35b439345c3cf9b50f3077e2
“anitapeppercorn” <anita@peppercorn.ai>
2021-09-12 20:36:00 -0700

    Updates

---
+++ precache-manifest.239a6f8456939325a7f18fbc2e592d1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27c67f724547ca6090e79127218e3d96",
+    "url": "/react-trello-example/index.html"
+  },
+  {
+    "revision": "f0a79a80e0f7b3b73dac",
+    "url": "/react-trello-example/static/css/main.4ea81e5a.chunk.css"
+  },
+  {
+    "revision": "1a2dcb9529d55058cf06",
+    "url": "/react-trello-example/static/js/2.b6516e89.chunk.js"
+  },
+  {
+    "revision": "43fda65c0fc967b18edaf33a1989f358",
+    "url": "/react-trello-example/static/js/2.b6516e89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0a79a80e0f7b3b73dac",
+    "url": "/react-trello-example/static/js/main.d99ebc44.chunk.js"
+  },
+  {
+    "revision": "41598ab83449e7c06444",
+    "url": "/react-trello-example/static/js/runtime-main.22d78beb.js"
+  }
+]);
